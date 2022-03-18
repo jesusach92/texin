661e3d36b69cdcdf7d00b786d85264f83a2f90a7
Jesus Alberto Castillo <jesusach92@gmail.com>
2022-03-18 14:04:24 -0600

    primer entrega

--- dist/controllers/controllers.js
+++ dist/controllers/controllers.js
@@ -5,7 +5,7 @@ var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefau
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.supplielist = exports.productlist = exports.addsupplie = exports.addproduct = void 0;
+exports.supplielist = exports.productlist = exports.getsuppliebyproduct = exports.getsuppliebyid = exports.getproductsbysupplie = exports.getproductbyid = exports.addsupplie = exports.addproduct = void 0;
 
 var _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator"));
 
@@ -21,6 +21,7 @@ function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (O
 
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { (0, _defineProperty2["default"])(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
 
+// Listar proveedores con contacto y domicilio principal
 var supplielist = /*#__PURE__*/function () {
   var _ref = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee(req, res) {
     var db, _yield$db$query, _yield$db$query2, rows;
@@ -35,15 +36,16 @@ var supplielist = /*#__PURE__*/function () {
           case 2:
             db = _context.sent;
             _context.next = 5;
-            return db.query("SELECT * FROM supplie");
+            return db.query("SELECT * FROM supplie t1 JOIN businesstype t2 JOIN adress_supplie t3 JOIN contact_supplies t4 ON t1.idBusinessType_Sup = t2.idbusinessType AND t1.id_supplie = t3.idSupplieAd AND t3.id_adress = t4.id_AdressCont WHERE t3.adress_principal = 1 AND t4.contact_principal =1 ORDER BY t1.id_supplie;");
 
           case 5:
             _yield$db$query = _context.sent;
             _yield$db$query2 = (0, _slicedToArray2["default"])(_yield$db$query, 1);
             rows = _yield$db$query2[0];
             res.json(rows);
+            db.end();
 
-          case 9:
+          case 10:
           case "end":
             return _context.stop();
         }
@@ -54,7 +56,8 @@ var supplielist = /*#__PURE__*/function () {
   return function supplielist(_x, _x2) {
     return _ref.apply(this, arguments);
   };
-}();
+}(); // Listar Productos
+
 
 exports.supplielist = supplielist;
 
@@ -72,15 +75,16 @@ var productlist = /*#__PURE__*/function () {
           case 2:
             db = _context2.sent;
             _context2.next = 5;
-            return db.query("SELECT * FROM products");
+            return db.query("SELECT * FROM products t1 JOIN technologies t2 ON t1.idTechnology_Pro = t2.id_technology;");
 
           case 5:
             _yield$db$query3 = _context2.sent;
             _yield$db$query4 = (0, _slicedToArray2["default"])(_yield$db$query3, 1);
             rows = _yield$db$query4[0];
             res.json(rows);
+            db.end();
 
-          case 9:
+          case 10:
           case "end":
             return _context2.stop();
         }
@@ -91,13 +95,15 @@ var productlist = /*#__PURE__*/function () {
   return function productlist(_x3, _x4) {
     return _ref2.apply(this, arguments);
   };
-}();
+}(); // Obtener Domicilios de Proveedores con contacto principal
+
 
 exports.productlist = productlist;
 
-var addsupplie = /*#__PURE__*/function () {
+var getsuppliebyid = /*#__PURE__*/function () {
   var _ref3 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee3(req, res) {
-    var db, result;
+    var db, _yield$db$query5, _yield$db$query6, rows;
+
     return _regenerator["default"].wrap(function _callee3$(_context3) {
       while (1) {
         switch (_context3.prev = _context3.next) {
@@ -108,12 +114,16 @@ var addsupplie = /*#__PURE__*/function () {
           case 2:
             db = _context3.sent;
             _context3.next = 5;
-            return db.query("INSERT INTO supplie (supplie_name, idBusinessType_Sup) VALUES (? , ?)", [req.body.supplie_name, req.body.idBusinessType_Sup]);
+            return db.query("select * from supplie t1 join businesstype t2 join adress_supplie t3 join contact_supplies t4 on t1.idBusinessType_Sup = t2.idbusinessType and t1.id_supplie = t3.idSupplieAd and t3.id_adress = t4.id_AdressCont where t1.id_supplie= ? AND t4.contact_principal =1 ORDER BY t3.adress_principal DESC;", [req.params.id]);
 
           case 5:
-            result = _context3.sent;
+            _yield$db$query5 = _context3.sent;
+            _yield$db$query6 = (0, _slicedToArray2["default"])(_yield$db$query5, 1);
+            rows = _yield$db$query6[0];
+            res.json(rows);
+            db.end();
 
-          case 6:
+          case 10:
           case "end":
             return _context3.stop();
         }
@@ -121,16 +131,17 @@ var addsupplie = /*#__PURE__*/function () {
     }, _callee3);
   }));
 
-  return function addsupplie(_x5, _x6) {
+  return function getsuppliebyid(_x5, _x6) {
     return _ref3.apply(this, arguments);
   };
-}();
+}(); //Productos por Proveedor
 
-exports.addsupplie = addsupplie;
 
-var addproduct = /*#__PURE__*/function () {
+exports.getsuppliebyid = getsuppliebyid;
+
+var getproductsbysupplie = /*#__PURE__*/function () {
   var _ref4 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee4(req, res) {
-    var db, _yield$db$query5, _yield$db$query6, result;
+    var db, _yield$db$query7, _yield$db$query8, rows;
 
     return _regenerator["default"].wrap(function _callee4$(_context4) {
       while (1) {
@@ -142,17 +153,16 @@ var addproduct = /*#__PURE__*/function () {
           case 2:
             db = _context4.sent;
             _context4.next = 5;
-            return db.query("INSERT INTO products (productName, description_product, idTechnology_Pro) values(?,?,?)", [req.body.productName, req.body.description_product, req.body.idTechnology_Pro]);
+            return db.query("SELECT * FROM supply t1 JOIN products t2 JOIN supplie t3 ON t1.idProduct_spy= t2.id_product  AND t1.idSupplie_spy = t3.id_supplie  WHERE t1.idSupplie_spy = ?;", [req.params.id]);
 
           case 5:
-            _yield$db$query5 = _context4.sent;
-            _yield$db$query6 = (0, _slicedToArray2["default"])(_yield$db$query5, 1);
-            result = _yield$db$query6[0];
-            res.json(_objectSpread({
-              id: result.insertId
-            }, req.body));
+            _yield$db$query7 = _context4.sent;
+            _yield$db$query8 = (0, _slicedToArray2["default"])(_yield$db$query7, 1);
+            rows = _yield$db$query8[0];
+            res.json(rows);
+            db.end();
 
-          case 9:
+          case 10:
           case "end":
             return _context4.stop();
         }
@@ -160,8 +170,159 @@ var addproduct = /*#__PURE__*/function () {
     }, _callee4);
   }));
 
-  return function addproduct(_x7, _x8) {
+  return function getproductsbysupplie(_x7, _x8) {
     return _ref4.apply(this, arguments);
+  };
+}();
+
+exports.getproductsbysupplie = getproductsbysupplie;
+
+var getproductbyid = /*#__PURE__*/function () {
+  var _ref5 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee5(req, res) {
+    var db, _yield$db$query9, _yield$db$query10, rows;
+
+    return _regenerator["default"].wrap(function _callee5$(_context5) {
+      while (1) {
+        switch (_context5.prev = _context5.next) {
+          case 0:
+            _context5.next = 2;
+            return (0, _database.connect)();
+
+          case 2:
+            db = _context5.sent;
+            _context5.next = 5;
+            return db.query("SELECT * FROM products WHERE id_product = ?", [req.params.id]);
+
+          case 5:
+            _yield$db$query9 = _context5.sent;
+            _yield$db$query10 = (0, _slicedToArray2["default"])(_yield$db$query9, 1);
+            rows = _yield$db$query10[0];
+            res.json(rows);
+            db.end();
+
+          case 10:
+          case "end":
+            return _context5.stop();
+        }
+      }
+    }, _callee5);
+  }));
+
+  return function getproductbyid(_x9, _x10) {
+    return _ref5.apply(this, arguments);
+  };
+}(); //Obtener Proveedores del Producto ID
+
+
+exports.getproductbyid = getproductbyid;
+
+var getsuppliebyproduct = /*#__PURE__*/function () {
+  var _ref6 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee6(req, res) {
+    var db, _yield$db$query11, _yield$db$query12, rows;
+
+    return _regenerator["default"].wrap(function _callee6$(_context6) {
+      while (1) {
+        switch (_context6.prev = _context6.next) {
+          case 0:
+            _context6.next = 2;
+            return (0, _database.connect)();
+
+          case 2:
+            db = _context6.sent;
+            _context6.next = 5;
+            return db.query("SELECT * FROM supply t1 JOIN products t2 JOIN supplie t3 JOIN technologies t4 ON t1.idProduct_spy = t2.id_product AND t2.idTechnology_Pro = t4.id_technology AND t1.idSupplie_spy = t3.id_supplie WHERE t1.idProduct_spy = ? ORDER BY t1.delivery_time and t1.price DESC;", [req.params.id]);
+
+          case 5:
+            _yield$db$query11 = _context6.sent;
+            _yield$db$query12 = (0, _slicedToArray2["default"])(_yield$db$query11, 1);
+            rows = _yield$db$query12[0];
+            res.json(rows);
+            db.end();
+
+          case 10:
+          case "end":
+            return _context6.stop();
+        }
+      }
+    }, _callee6);
+  }));
+
+  return function getsuppliebyproduct(_x11, _x12) {
+    return _ref6.apply(this, arguments);
+  };
+}();
+
+exports.getsuppliebyproduct = getsuppliebyproduct;
+
+var addsupplie = /*#__PURE__*/function () {
+  var _ref7 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee7(req, res) {
+    var db, result;
+    return _regenerator["default"].wrap(function _callee7$(_context7) {
+      while (1) {
+        switch (_context7.prev = _context7.next) {
+          case 0:
+            _context7.next = 2;
+            return (0, _database.connect)();
+
+          case 2:
+            db = _context7.sent;
+            _context7.next = 5;
+            return db.query("INSERT INTO supplie (supplie_name, idBusinessType_Sup) VALUES (? , ?)", [req.body.supplie_name, req.body.idBusinessType_Sup]);
+
+          case 5:
+            result = _context7.sent;
+            db.end();
+
+          case 7:
+          case "end":
+            return _context7.stop();
+        }
+      }
+    }, _callee7);
+  }));
+
+  return function addsupplie(_x13, _x14) {
+    return _ref7.apply(this, arguments);
+  };
+}();
+
+exports.addsupplie = addsupplie;
+
+var addproduct = /*#__PURE__*/function () {
+  var _ref8 = (0, _asyncToGenerator2["default"])( /*#__PURE__*/_regenerator["default"].mark(function _callee8(req, res) {
+    var db, _yield$db$query13, _yield$db$query14, result;
+
+    return _regenerator["default"].wrap(function _callee8$(_context8) {
+      while (1) {
+        switch (_context8.prev = _context8.next) {
+          case 0:
+            _context8.next = 2;
+            return (0, _database.connect)();
+
+          case 2:
+            db = _context8.sent;
+            _context8.next = 5;
+            return db.query("INSERT INTO products (productName, description_product, idTechnology_Pro) values(?,?,?)", [req.body.productName, req.body.description_product, req.body.idTechnology_Pro]);
+
+          case 5:
+            _yield$db$query13 = _context8.sent;
+            _yield$db$query14 = (0, _slicedToArray2["default"])(_yield$db$query13, 1);
+            result = _yield$db$query14[0];
+            res.json(_objectSpread({
+              id: result.insertId
+            }, req.body));
+            db.end();
+
+          case 10:
+          case "end":
+            return _context8.stop();
+        }
+      }
+    }, _callee8);
+  }));
+
+  return function addproduct(_x15, _x16) {
+    return _ref8.apply(this, arguments);
   };
 }();
 
--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -5,12 +5,14 @@ export const supplielist = async (req, res) => {
     const db= await connect();
     const [rows] = await db.query("SELECT * FROM supplie t1 JOIN businesstype t2 JOIN adress_supplie t3 JOIN contact_supplies t4 ON t1.idBusinessType_Sup = t2.idbusinessType AND t1.id_supplie = t3.idSupplieAd AND t3.id_adress = t4.id_AdressCont WHERE t3.adress_principal = 1 AND t4.contact_principal =1 ORDER BY t1.id_supplie;");
     res.json(rows);
+    db.end()
 }
 // Listar Productos
 export const productlist = async (req, res) => {
     const db= await connect();
     const [rows] = await db.query("SELECT * FROM products t1 JOIN technologies t2 ON t1.idTechnology_Pro = t2.id_technology;");
-    res.json(rows);
+    res.json(rows)
+    db.end()
 }
 // Obtener Domicilios de Proveedores con contacto principal
 export const getsuppliebyid = async (req, res) => {
@@ -18,14 +20,17 @@ export const getsuppliebyid = async (req, res) => {
     const [rows] = await db.query("select * from supplie t1 join businesstype t2 join adress_supplie t3 join contact_supplies t4 on t1.idBusinessType_Sup = t2.idbusinessType and t1.id_supplie = t3.idSupplieAd and t3.id_adress = t4.id_AdressCont where t1.id_supplie= ? AND t4.contact_principal =1 ORDER BY t3.adress_principal DESC;",[
         req.params.id]);
     res.json(rows);
+    db.end()
 }
 
+//Productos por Proveedor
 export const getproductsbysupplie = async (req, res) => {
     const db= await connect();
-    const [rows] = await db.query("SELECT supplie_name,productName,price,delivery_time,comments FROM supplies.supply inner join products inner join supplie on idProduct_spy= id_product and idSupplie_spy = id_supplie where supply.idSupplie_spy = ?;",[
+    const [rows] = await db.query("SELECT * FROM supply t1 JOIN products t2 JOIN supplie t3 ON t1.idProduct_spy= t2.id_product  AND t1.idSupplie_spy = t3.id_supplie  WHERE t1.idSupplie_spy = ?;",[
         req.params.id
     ])
     res.json(rows);
+    db.end()
 }
 
 export const getproductbyid = async (req, res) => {
@@ -33,23 +38,26 @@ export const getproductbyid = async (req, res) => {
     const [rows] = await db.query("SELECT * FROM products WHERE id_product = ?",[
         req.params.id]);
     res.json(rows);
+    db.end()
 }
 
 //Obtener Proveedores del Producto ID
 export const getsuppliebyproduct = async (req, res) => {
     const db= await connect();
-    const [rows] = await db.query("SELECT id_supplie, supplie_name, productName, delivery_time, price, product_line FROM supply inner join supplie inner join products on idSupplie_spy = id_supplie and idProduct_spy = id_product where idProduct_spy = ?;",[
+    const [rows] = await db.query("SELECT * FROM supply t1 JOIN products t2 JOIN supplie t3 JOIN technologies t4 ON t1.idProduct_spy = t2.id_product AND t2.idTechnology_Pro = t4.id_technology AND t1.idSupplie_spy = t3.id_supplie WHERE t1.idProduct_spy = ? ORDER BY t1.delivery_time and t1.price DESC;",[
     req.params.id]);
     res.json(rows);
+    db.end()
 }
-
 
 export const addsupplie = async (req, res) => {
     const db= await connect();
-    const result = await db.query("INSERT INTO supplie (supplie_name, idBusinessType_Sup) VALUES (? , ?)",[
+        const result = await db.query("INSERT INTO supplie (supplie_name, idBusinessType_Sup) VALUES (? , ?)",[
         req.body.supplie_name,
         req.body.idBusinessType_Sup
+    
     ]);
+    db.end()
 }
 export const addproduct = async (req, res) => {
     const db= await connect();
@@ -63,4 +71,5 @@ export const addproduct = async (req, res) => {
         id: result.insertId,
         ...req.body
     });
+    db.end()
 }
