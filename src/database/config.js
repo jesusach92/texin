38c28a57eed8f738b965a908baefada74be48fd4
Jesus Alberto Castillo <jesusach92@gmail.com>
2022-04-22 10:20:58 -0500

    Avance SERVER

--- src/database/config.js
+++ src/database/config.js
@@ -3,7 +3,9 @@ dotenv.config()
 
 export const config = {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
+
 };
