07ca9618d2bb73d9ed969483a6bfb3d767499354
Jesus Alberto Castillo <jesusach92@gmail.com>
2022-06-30 11:45:51 -0500

    Avance

--- src/controllers/files.js
+++ src/controllers/files.js
@@ -4,59 +4,61 @@ import fs from "fs";
 import { connect } from "../database/database";
 
 const directory = (supplie) => {
-  const pathFiles = path.join(__dirname, `catalogs/${supplie}`);
-  if (fs.existsSync(pathFiles)) {
-    return pathFiles;
-  } else {
-    fs.mkdir(pathFiles, (err) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("satisfactory");
-    });
-    return pathFiles;
-  }
+	const pathFiles = path.join(__dirname, `catalogs/${supplie}`);
+	if (fs.existsSync(pathFiles)) {
+		return pathFiles;
+	} else {
+		fs.mkdir(pathFiles, (err) => {
+			if (err) {
+				return console.log(err);
+			}
+			console.log("satisfactory");
+		});
+		return pathFiles;
+	}
 };
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const { supplie } = req.body;
-    const directoryFile = directory(supplie);
-    cb(null, `${directoryFile}`);
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-    );
-  },
+	destination: (req, file, cb) => {
+		const { supplie } = req.body;
+		const directoryFile = directory(supplie);
+		cb(null, `${directoryFile}`);
+	},
+	filename: (req, file, cb) => {
+		cb(
+			null,
+			file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+		);
+	},
 });
 export const uploadFile = multer({
-  storage,
-  fileFilter: (req, file, cb) => {
-    if (!file.originalname.match(/\.(pdf|doc|docx|jpg)$/)) {
-      return cb(new Error("Error en el tipo de archivo"));
-    }
-    cb(null, true);
-  },
+	storage,
+	fileFilter: (req, file, cb) => {
+		if (!file.originalname.match(/\.(pdf|doc|docx|jpg)$/)) {
+			return cb(new Error("Error en el tipo de archivo"));
+		}
+		cb(null, true);
+	},
 });
 
 export const fileUp = async (req, res) => {
-    try {
-        const db = await connect();
-        const {rows} = await db.query("UPDATE supplie set catalog=? where idSupplie=?",[
-            req.file.path,
-            req.body.supplie
-        ]) 
-        console.log(rows)
-res.status(200).json({ message: "Exito" });
-    } catch (error) {
-        console.log(error)
-        res.status(400).send("Error")
-    }
+		try {
+				const db = await connect();
+				const [,pathFile] = req.file.path.split("catalogs")
+				console.log(pathFile);
+				const {rows} = await db.query("UPDATE supplie set catalog=? where idSupplie=?",[
+						pathFile,
+						req.body.supplie
+				]) 
+res.status(200).json({ message: "Catalogo Guardado correctamente" });
+		} catch (error) {
+				console.log(error)
+				res.status(400).send("Error")
+		}
 };
 
 export const getFile = async (req, res) => {
-  res
-    .status(200)
-    .download(__dirname + "/catalogs/15/catalogo-1656524038866.docx");
+	const [,supplie,pathFile] = req.url.split("%5C")
+	res
+		.status(200)
+		.download(__dirname + `/catalogs/${supplie}/${pathFile}`);
 };
--- src/controllers/supplies.js
+++ src/controllers/supplies.js
@@ -1,136 +1,136 @@
 import { connect } from "../database/database";
 
 export async function UpdateSup(Id, db) {
-  try {
-    await db.query(
-      "UPDATE supplie SET  supplie.sDateUpdate=? WHERE idSupplie=?;",
-      [date(), Id]
-    );
-  } catch (e) {
-    console.log(e);
-  }
+	try {
+		await db.query(
+			"UPDATE supplie SET  supplie.sDateUpdate=? WHERE idSupplie=?;",
+			[date(), Id]
+		);
+	} catch (e) {
+		console.log(e);
+	}
 }
 
 // Fecha de ultima actualizacion
 export const date = () => {
-  let fulldate = new Date();
-  let date = `${fulldate.getFullYear()}-${
-    fulldate.getMonth() + 1
-  }-${fulldate.getDate()}`;
-  return date;
+	let fulldate = new Date();
+	let date = `${fulldate.getFullYear()}-${
+		fulldate.getMonth() + 1
+	}-${fulldate.getDate()}`;
+	return date;
 };
 /* Metodos de los Proveedores*/
 
 // 1.- Listar proveedores con el tipo de negocio y el tipo de proveedor que son
 export const supplielist = async (req, res) => {
-  try {
-    const db = await connect();
-    const [rows] = await db.query(
-      "SELECT * FROM supplie t1 JOIN businesstype t2 JOIN sclasification t3 ON t1.FkBusinessType=t2.idBusinessType AND t1.FkClasification=t3.idClasification ORDER BY sDateUpdate DESC;"
-    );
-    if (!rows) {
-      res.json([]);
-    } else {
-      res.json(rows);
-    }
-    db.end();
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		const db = await connect();
+		const [rows] = await db.query(
+			"SELECT * FROM supplie t1 JOIN businesstype t2 JOIN sclasification t3 ON t1.FkBusinessType=t2.idBusinessType AND t1.FkClasification=t3.idClasification ORDER BY sDateUpdate DESC;"
+		);
+		if (!rows) {
+			res.json([]);
+		} else {
+			res.json(rows);
+		}
+		db.end();
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 // 2.-Obtener Proveedor por Id
 
 export const supplieById = async (req, res) => {
-  const db = await connect();
-  try {
-    const [rows] = await db.query(
-      "SELECT * FROM supplie t1 JOIN businesstype t2 JOIN sclasification t3 ON t1.FkBusinessType = t2.idBusinessType AND t1.FkClasification = t3.idClasification WHERE t1.idSupplie= ?;",
-      [req.params.id]
-    );
-    if (!rows.length) res.json([]);
-    else res.json(rows);
-  } catch (e) {
-    console.log(e);
-  }
-  db.end();
+	const db = await connect();
+	try {
+		const [rows] = await db.query(
+			"SELECT * FROM supplie t1 JOIN businesstype t2 JOIN sclasification t3 ON t1.FkBusinessType = t2.idBusinessType AND t1.FkClasification = t3.idClasification WHERE t1.idSupplie= ?;",
+			[req.params.id]
+		);
+		if (!rows.length) res.json([]);
+		else res.json(rows);
+	} catch (e) {
+		console.log(e);
+	}
+	db.end();
 };
 
 //3.-Agregar Proveedor
 export const addSupplie = async (req, res) => {
-  try {
-    const db = await connect();
-    const [[[{ idSupplie }]]] = await db.query(
-      "Call InSupplie(?,?,?,?,?,?,?,?,?,?,?)",
-      [
-        req.body.nameSupplie,
-        req.body.FkBusinessType,
-        req.body.FkClasification,
-        date(),
-        date(),
-        req.body.emailSupplie,
-        req.body.contactPhone,
-        req.body.userRegister,
-        req.body.userUpdate,
-        req.body.webPage,
-        req.body.catalog
-      ]
-    );
-    res.json({ idSupplie, value: 1 });
-    db.end();
-  } catch (e) {
-    console.log(e);
-    res.json({ value: 0 });
-  }
+	try {
+		const db = await connect();
+		const[[[{idSupplie}]]]= await db.query(
+			"Call InSupplie(?,?,?,?,?,?,?,?,?,?,?);",
+			[
+				req.body.nameSupplie,
+				req.body.FkBusinessType,
+				req.body.FkClasification,
+				date(),
+				date(),
+				req.body.emailSupplie,
+				req.body.contactPhone,
+				req.body.userRegister,
+				req.body.userUpdate,
+				req.body.webPage,
+				req.body.catalog
+			]
+		);
+		res.json({ idSupplie, value: 1 });
+		db.end();
+	} catch (e) {
+		console.log(e);
+		res.json({ value: 0 });
+	}
 };
 
 //4.-Editar Proveedor
 export const EditSupplie = async (req, res) => {
-  try {
-    const db = await connect();
-    const [[[rows]]] = await db.query(
-      "Call updateSupplie(?,?,?,?,?,?,?,?,?);",
-      [
-        req.body.idSupplie,
-        req.body.nameSupplie,
-        req.body.FkBusinessType,
-        req.body.FkClasification,
-        date(),
-        req.body.emailSupplie,
-        req.body.contactPhone,
-        req.body.userUpdate,
-        req.body.webPage,
-      ]
-    );
-    if (rows?.message) {
-      res.json({
-        value: 1,
-        message: "La actualizacion fue realizada correctamente",
-      });
-    } else {
-      res.json({ value: 0, messaje: "No se realizo la actualizacion" });
-    }
-    db.end();
-  } catch (e) {
-    console.log(e);
-  }
+	try {
+		const db = await connect();
+		const [[[rows]]] = await db.query(
+			"Call updateSupplie(?,?,?,?,?,?,?,?,?);",
+			[
+				req.body.idSupplie,
+				req.body.nameSupplie,
+				req.body.FkBusinessType,
+				req.body.FkClasification,
+				date(),
+				req.body.emailSupplie,
+				req.body.contactPhone,
+				req.body.userUpdate,
+				req.body.webPage,
+			]
+		);
+		if (rows?.message) {
+			res.json({
+				value: 1,
+				message: "La actualizacion fue realizada correctamente",
+			});
+		} else {
+			res.json({ value: 0, messaje: "No se realizo la actualizacion" });
+		}
+		db.end();
+	} catch (e) {
+		console.log(e);
+	}
 };
 
 //5.-Eliminar Proveedor, eliminar por completo todos los datos relacionados con el proveedor, domicilios, contactos, productos que tenga asignados.
 export const deleteSupplie = async (req, res) => {
-  try {
-    const db = await connect();
-    const [rows] = await db.query("DELETE FROM supplie WHERE idSupplie=?;", [
-      req.params.id,
-    ]);
-    if (rows.affectedRows > 0) {
-      res.send("Proveedor Eliminado con Exito");
-    } else {
-      res.send("No se pudo eliminar el proveedor");
-    }
-    db.end();
-  } catch (e) {
-    console.log(e);
-  }
+	try {
+		const db = await connect();
+		const [rows] = await db.query("DELETE FROM supplie WHERE idSupplie=?;", [
+			req.params.id,
+		]);
+		if (rows.affectedRows > 0) {
+			res.send("Proveedor Eliminado con Exito");
+		} else {
+			res.send("No se pudo eliminar el proveedor");
+		}
+		db.end();
+	} catch (e) {
+		console.log(e);
+	}
 };
 
